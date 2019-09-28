d7821fdd9951d0a1d9c71bc7a3c70d81d2b0540b
Zain <zain.sayed6@gmail.com>
2019-09-28 01:27:27 -0400

    Grid generation complete

--- pygameBase.py
+++ pygameBase.py
@@ -1,14 +1,7 @@
 """
- Pygame base template for opening a window
-
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
-
- Explanation video: http://youtu.be/vRB_983kUMc
+ @author:zain
 """
-
+import random
 import pygame
 
 # Define some colors
@@ -16,19 +9,52 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+#setting height and width of cells
+BLOCK_WIDTH = 5
+BLOCK_HEIGHT = 5
+#Setting margin
+MARGIN = 0
+
+grid = []
+grid_height = 101
+    #int(input("Input a grid height: "))
+grid_width = 101
+    #int(input("Input a grid width:  "))
+
+for row in range(grid_height):
+    grid.append([])
+    for column in range(grid_width):
+        coinFlip = random.randint(0, 100)
+        if(coinFlip>24):
+            grid[row].append(0)
+        else:
+            grid[row].append(1)
+
+rand_start_x = random.randint(0,101)
+rand_start_y = random.randint(0,101)
+rand_goal_x = random.randint(0,101)
+rand_goal_y = random.randint(0,101)
+print(rand_start_x,  rand_start_y)
+print(rand_goal_x,rand_goal_y)
+grid[rand_start_x][rand_start_y] = 2
+grid[rand_goal_x][rand_goal_y] = 3
+
+        #The zero/one here is appending a cell
+
+
 
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (700, 500)
+size = (505, 505)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("A* Grid")
 
-# Loop until the user clicks the close button.
+
 done = False
-
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
@@ -37,6 +63,33 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            column = pos[0]
+            row = pos[1]
+            print(pos)
+
+
+    screen.fill(BLACK)
+
+    for row in range(grid_height):
+        for column in range(grid_width):
+
+            if (grid[row][column] == 0):
+                color = WHITE
+            elif (grid[row][column] == 2):
+                color = GREEN
+                #Starting point is GREEN
+            elif (grid[row][column] == 3):
+                color = RED
+                #Goal is RED
+            else:
+                color = BLACK
+
+            pygame.draw.rect(screen,color,[BLOCK_WIDTH*column,BLOCK_HEIGHT*row,BLOCK_WIDTH,BLOCK_HEIGHT])
+
+
+
 
     # --- Game logic should go here
 
@@ -47,7 +100,7 @@ while not done:
 
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(WHITE)
+
 
     # --- Drawing code should go here
 
@@ -55,7 +108,7 @@ while not done:
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick(4)
 
 # Close the window and quit.
 pygame.quit()
