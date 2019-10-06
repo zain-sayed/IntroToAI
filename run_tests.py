51c0e5d5dc038a5ec2b0db2ff3d580200090e8c8
Ernest Lin <ernesthlin@gmail.com>
2019-10-06 17:39:41 -0400

    edited file writer

--- run_tests.py
+++ run_tests.py
@@ -5,15 +5,23 @@ import xlwt
 import time
 import random
 
-NUM_TIMES = 10
+NUM_TIMES = 1
 
 def main():
 	book = xlwt.Workbook()
 	fwd_sheet = book.add_sheet("Foward")
+	bwd_sheet = book.add_sheet("Backward")
+	adpt_sheet = book.add_sheet("Adaptive")
 	fwd_sheet.write(0, 0, "Observation")
 	fwd_sheet.write(0, 1, "Time")
 	fwd_sheet.write(0, 2, "Cells Expanded")
-	random.seed(0)
+	bwd_sheet.write(0, 0, "Observation")
+	bwd_sheet.write(0, 1, "Time")
+	bwd_sheet.write(0, 2, "Cells Expanded")
+	adpt_sheet.write(0, 0, "Observation")
+	adpt_sheet.write(0, 1, "Time")
+	adpt_sheet.write(0, 2, "Cells Expanded")
+	random.seed(1)
 	for i in range(NUM_TIMES):
 		pygame.init()
 		# start coords
@@ -29,12 +37,24 @@ def main():
 		# initialize grid
 		grid = gridFunc.gridInit(startCoord, goalCoord)
 
-		cells_explored, time_stamp = astar.repeatForwardAStar(pygame, grid, startCoord, goalCoord, time)
+		cells_explored, time_stamp = astar.repeatedForwardAstar(pygame, grid, startCoord, goalCoord, time)
 		fwd_sheet.write(i + 1, 0, i + 1)
 		fwd_sheet.write(i + 1, 1, time_stamp)
 		fwd_sheet.write(i + 1, 2, cells_explored)
+
+		cells_explored, time_stamp = astar.repeatedBackwardAstar(pygame, grid, startCoord, goalCoord, time)
+		bwd_sheet.write(i + 1, 0, i + 1)
+		bwd_sheet.write(i + 1, 1, time_stamp)
+		bwd_sheet.write(i + 1, 2, cells_explored)
+
+		cells_explored, time_stamp = astar.adaptive_repeatedForwardAstar(pygame, grid, startCoord, goalCoord, time)
+		adpt_sheet.write(i + 1, 0, i + 1)
+		adpt_sheet.write(i + 1, 1, time_stamp)
+		adpt_sheet.write(i + 1, 2, cells_explored)
+
+
 		pygame.quit()
-	book.save("Results.xlsx")
+	book.save("Results.xls")
 
 
 main()
